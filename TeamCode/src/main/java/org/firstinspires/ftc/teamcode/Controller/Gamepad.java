595d82668301ecc7b16a8abf2e479d07e7e26534
Akash <akash.moorching@gmail.com>
2021-09-09 19:27:44 -0500

    created the outtake file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controller/Gamepad.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Controller;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Gamepad {
+
+        DcMotor front_left_motor;
+        DcMotor front_right_motor;
+        DcMotor back_right_motor;
+        DcMotor back_left_motor;
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subystems/SSOuttake1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subystems/SSOuttake1.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Subystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class SSOuttake1 {
@@ -19,10 +20,15 @@ public class SSOuttake1 {
     }
 
     public void startOuttake(){
+        upperOuttakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        lowerOuttakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        upperOuttakeMotor.setPower(0.5);
+        lowerOuttakeMotor.setPower(0.5);
     }
 
     public void stopOuttake(){
-
+        upperOuttakeMotor.setPower(0);
+        lowerOuttakeMotor.setPower(0);
     }
 }
